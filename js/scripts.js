03915e7f4ece504b287c6c828951854f1e554c6a
Cheshire Cat <thacheshirecat@gmail.com>
2018-06-08 10:24:42 -0700

    Add functionallity to replace any number evenly divisible by 3 into Error.

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,22 @@ var createArray = function(input)
   var firstArray = [];
   for (index = input; index >= 0; index--)
   {
-    firstArray.push(input.toString());
-    input = input - 1;
+    if (input === 0)
+    {
+      firstArray.push(input.toString());
+      input = input - 1;
+    }
+    else if (input%3 === 0)
+    {
+      firstArray.push("Error.");
+      input = input - 1;
+    }
+    else
+    {
+      firstArray.push(input.toString());
+      input = input - 1;
+    }
+
   }
   firstArray.reverse();
   return firstArray;
@@ -19,9 +33,7 @@ var createArraysArray = function(array)
   array.forEach(function(element)
   {
     element = element.split("");
-    alert(element);
   });
-  alert(array);
   return array;
 };
 
@@ -54,11 +66,15 @@ $(document).ready(function()
   $("#number-input").submit(function(event)
   {
     event.preventDefault();
+    $("#results").text("");
 
     var userInput = parseInt($("#the-number").val());
     var firstArray = createArray(userInput);
-    var result = createArraysArray(firstArray);
-    alert(result);
+    var results = createArraysArray(firstArray);
+    results.forEach(function(result)
+    {
+      $("#results").append("<li>"+result+"</li>");
+    })
 
   });
 
